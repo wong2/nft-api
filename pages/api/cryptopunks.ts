7e5a4eec26a7100e7cf7a4c9b902f1f721fe9f03
wong2 <wonderfuly@gmail.com>
2022-06-27 17:53:38 +0800

    Add cryptopunks api

---
+++ pages/api/cryptopunks.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as cheerio from "cheerio";
+import axios from "axios";
+
+async function fetchCryptoPunksStats() {
+  const { data: html } = await axios.get("https://www.larvalabs.com/cryptopunks");
+  const $ = cheerio.load(html);
+  const [floorPrice, _, __, daySales] = $(".punk-stat")
+    .map((_i, el) => {
+      const splitted = $(el).text().trim().split("\n");
+      const text = splitted[splitted.length - 1].trim();
+      const [t] = text.split(/ETH|Ξ/);
+      return t.endsWith("K") ? parseFloat(t) * 1000 : parseFloat(t);
+    })
+    .toArray();
+  return {
+    floor_price: floorPrice,
+    one_day_volume: daySales,
+  };
+}
+
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+  const stats = await fetchCryptoPunksStats();
+  res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
+  res.status(200).json(stats);
+}
